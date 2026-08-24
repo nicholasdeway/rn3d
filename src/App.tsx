4e38d9db98584cd0067cb69b2c85d108fb06127b
Nicholas Deway <nicholasdeway@hotmail.com>
2026-08-24 04:08:55 -0300

    fix: Adicionado import do hook useMemo no topo do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Sidebar } from './components/Sidebar';
 import { Header } from './components/Header';
 import { Toast } from './components/Toast';
